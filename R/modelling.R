0fb49b0088856c299c616eaf3df2c8492782b4d3
Marcus Groß <marcus.gross@inwt-statistics.de>
2020-07-14 18:22:07 +0200

    Update rstan sampling and model

--- R/modelling.R
+++ R/modelling.R
@@ -1,13 +1,13 @@
 #' @export
-compileRunModel <- function(data, iter = 800, warmup = 500,
+compileRunModel <- function(data, iter = 600, warmup = 400,
                             control = list(max_treedepth = 8, adapt_delta = 0.8),
-                            chains = 4, cores = 4, seed = 12345){
-  mpModel <- stan_model(file = "stan_models/lsModelMultiT6Fast.stan")
+                            chains = 5, cores = 5, seed = 12345){
+  mpModel <- rstan::stan_model(file = "stan_models/lsModelMultiT6Fast.stan")
 
-  f <- sampling(mpModel, data = data,
+  f <- rstan::sampling(mpModel, data = data,
                 init_r = 0.1,
-                pars = c("y", "alpha", "theta", "theta2", "phi", "opposition",
-                         "government", "epsilon", "mu","tau", "tau2"),
+                # pars = c("y", "alpha", "theta", "theta2", "phi", "opposition",
+                #          "government", "epsilon", "mu","tau", "tau2"),
                 iter= iter, warmup = warmup, chains = chains, cores = cores,
                 seed = seed,
                 control = control)
