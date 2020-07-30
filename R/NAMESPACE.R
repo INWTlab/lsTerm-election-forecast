61bf43ed847c1f435c6cc3d7da5e54eafb82c7e4
Marcus Gross <marcus.gross@inwt-statistics.de>
2020-07-30 11:19:42 +0200

    Namespace update

--- R/NAMESPACE.R
+++ R/NAMESPACE.R
@@ -1,6 +1,6 @@
 #' @importFrom data.table rbindlist
 #' @importFrom dplyr %>% arrange filter group_by slice ungroup mutate as_tibble select mutate_each desc funs n bind_rows
-#' @importFrom ggplot2 ggplot geom_ribbon aes geom_line geom_vline xlim annotate geom_point ylim scale_fill_manual geom_point scale_color_manual
+#' @importFrom ggplot2 ggplot geom_ribbon aes geom_line geom_vline xlim annotate geom_point ylim scale_fill_manual geom_point scale_color_manual scale_y_continuous
 #' @importFrom rstan sampling stan_model extract
 #' @importFrom tidyr gather
 #' @importFrom rvest html_nodes html_text
