5adcaf41b5365c3ead11fe3c9ff8e03f56ab168f
Mira Céline Klein <mira.klein@inwt-statistics.de>
2024-04-29 11:17:27 +0200

    Readme and docs

--- inst/R_Scripts/scriptMulti.R
+++ inst/R_Scripts/scriptMulti.R
@@ -15,9 +15,8 @@ sessionInfo()
 
 # Get and prepare data ----
 
-dataDE <- getDataDE(predDate)
-fact_survey <- getFactSurvey(dataDE$pollData)
-
+dataDE <- getDataDE(predDate) # list of poll data and elections data
+fact_survey <- getFactSurvey(dataDE$pollData) # poll data
 printLatestPollPerPollster(fact_survey)
 
 dataPrep <-
@@ -33,12 +32,20 @@ modelResults <- compileRunModel(data = dataPrep$modelData, dev = dev)
 plotForecast <- plotElectionData(modelResults, dataPrep, predDate,
                                  dataDE$pollData, start = "2020-01-01")
 print(plotForecast$plot)
+
+# forecasts for all political parties
 fact_forecast <-
   prepareForecastTable(modelResults, dataPrep, predDate)
+
+# probabilities of events taking in consideration election forecasts
 fact_event_prob <- eventsDE(modelResults, dataPrep, predDate)
+
+# probabilities of governing coalitions
 fact_coalition_prob <-
   calculate_coalition_probs(modelResults, dataPrep, predDate)
+
+# probabilities that each party is part of the government
 fact_part_of_government <-
   partOfGovernmentDE(fact_coalition_prob, predDate)
+
 fact_forecast_all <- getForecastAllTable(plotForecast$plotData)
-fact_coalition_prob$possible_coalition <- NULL
